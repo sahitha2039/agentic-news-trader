1d1d1b0c4b62b9c8f34b2da02552d90d0d83fce8
sahitha2039 <sahitha.sc@gmail.com>
2026-03-22 23:55:07 -0400

    Moved analyzer initialization outside of function call in sentiment.py, added compound and label to main.py

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ from sentiment import *
 articles = fetch_news()
 
 for i, article in enumerate(articles, start=1):
+
     title = article.get('title')
     result = analyze_sentiment(title)
-    print(result)
+    compound = result.get('compound')
+
+    if compound >= 0.5:
+        label = "POSITIVE"
+
+    elif compound <= -0.5:
+        label = "NEGATIVE"
+
+    else:
+        label = "NEUTRAL"
+
+    print(f"{i}. {title}")
+    print(f"    Source: {article.get('source', '')}")
+    print(f"    Sentiment score: {compound}")
+    print(f"    Label: {label}")
--- sentiment.py
+++ sentiment.py
@@ -2,8 +2,9 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import nltk
 
 nltk.download('vader_lexicon')
+analyzer = SentimentIntensityAnalyzer()
+
 
 def analyze_sentiment(text):
-    analyzer = SentimentIntensityAnalyzer()
     sentiment_score = analyzer.polarity_scores(text)
     return sentiment_score

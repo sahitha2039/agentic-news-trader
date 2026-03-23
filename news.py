43ff6a8db2fbe610d0eb9b09cbc83f9586b5274f
sahitha2039 <sahitha.sc@gmail.com>
2026-03-22 21:59:56 -0400

    Fixed print statement to make it less likely to crash in case of invalid input

--- news.py
+++ news.py
@@ -9,13 +9,15 @@ api_key = os.getenv("NEWS_API_KEY")
 url = "https://newsapi.org/v2/everything"
 headers = { "X-API-Key": api_key}
 params = {
-    "q": "stock market OR stocks OR finance OR economy OR earnings OR market OR trading"
+    "q": "stock market OR stocks OR finance OR economy OR earnings OR market OR trading",
+    "pageSize": 20,
+    "sortBy": "publishedAt"
 }
 response = requests.get(url, headers=headers, params=params)
 
 if response.status_code == 200:
     data = response.json()
     for i, article in enumerate(data.get("articles", []), start=1):
-        print(f"{i}. {article['title']} ({article['source']['name']}) \n")
+        print(f"{i}. {article.get('title')} {article.get('source', {}).get('name')} \n")
 else:
     print(f"Error: {response.status_code}")

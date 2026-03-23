7204990163df45717b37348f640f519558b9f210
sahitha2039 <sahitha.sc@gmail.com>
2026-03-22 21:10:05 -0400

    Moved .env out of .gitignore folder, made .gitignore file with .env in it, fixed api fetching code (was an error in api key), feature now properly fetches data from the api

--- news.py
+++ news.py
@@ -1,6 +1,20 @@
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 
 api_key = os.getenv("NEWS_API_KEY")
+
+url = "https://newsapi.org/v2/everything"
+headers = { "X-API-Key": api_key}
+params = {
+    "q": "stock market", 
+}
+response = requests.get(url, headers=headers, params=params)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Error: {response.status_code}")
